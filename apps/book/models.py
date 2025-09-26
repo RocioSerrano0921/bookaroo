116faea7de6e547ceeb3bb507d43e08578089765
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-09-26 15:31:55 +0100

    Add initial migration, models for Author, and update settings for database configuration

--- apps/book/models.py
+++ apps/book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    fullname = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.fullname
