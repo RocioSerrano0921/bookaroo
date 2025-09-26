21dd305092751df279e4e4f07556e27e8e75558c
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-09-26 12:25:05 +0100

    Create book app and  Procfile for deployment with Gunicorn

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l7a!si!2n3@x)1ih16z3-mq3qlsonaib#j$^5wav4ckj8b*140'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bookaroo.herokuapp.com"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.book',
 ]
 
 MIDDLEWARE = [
