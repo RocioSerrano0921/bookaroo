eba4db92efec0bef38a7f86b52f45b464be9e4eb
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-09-26 16:41:17 +0100

    Add security

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l7a!si!2n3@x)1ih16z3-mq3qlsonaib#j$^5wav4ckj8b*140'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
