ed5f26e9738e583d23ff85031c85da4fbb0d2e23
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-09-30 17:23:31 +0100

    Add CRUD Class views-Add author_confirm_delete.html template

--- apps/book/urls.py
+++ apps/book/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import create_author, delete_author, ListAuthor, edit_author
+from .views import CreateAuthor, ListAuthor, EditAuthor, DeleteAuthor
 
 urlpatterns = [
-    path('create_author/', create_author, name='create_author'),
+    path('create_author/', CreateAuthor.as_view(), name='create_author'),
     path('list_authors/', ListAuthor.as_view(), name='list_authors'),
-    path('edit_author/<int:author_id>/', edit_author, name='edit_author'),
-    path('delete_author/<int:author_id>/', delete_author, name='delete_author'),
+    path('edit_author/<int:pk>/', EditAuthor.as_view(), name='edit_author'),
+    path('delete_author/<int:pk>/', DeleteAuthor.as_view(), name='delete_author'),
 ]
