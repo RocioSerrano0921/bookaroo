82709fc1fdb93e8b72a072c79b235bb5b6eb42c7
RocioSerrano0921 <rocioserranogomez21@gmail.com>
2025-10-02 14:13:33 +0100

    Add LOGGING to output Django errors to console in production

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "ERROR"},
+        "django.request": {"handlers": ["console"], "level": "ERROR", "propagate": False},
+    },
+}
